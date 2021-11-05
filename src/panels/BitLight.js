4687d6cbd88437a91ed402af7857197afc54e29f
Lambo-555 <lambo-555@yandex.ru>
2021-11-05 17:09:49 +0300

    update

--- src/panels/BitLight.js
+++ src/panels/BitLight.js
@@ -1,20 +1,42 @@
 import React, {useState} from 'react';
-import {Panel, PanelHeader, PanelHeaderBack, Group, Header, Div, Button, IconButton, Headline} from "@vkontakte/vkui";
+import {
+  Panel,
+  PanelHeader,
+  PanelHeaderBack,
+  Group,
+  Header,
+  Div,
+  Button,
+  IconButton,
+  Headline,
+  List,
+  Cell
+} from "@vkontakte/vkui";
 import {Icon28VinylOutline, Icon16Add, Icon16Cancel, Icon28Play, Icon24Stop} from '@vkontakte/icons';
 import bridge from "@vkontakte/vk-bridge";
 import {useEffect} from "react";
 
 const BitLight = props => {
-  const [bpsList, setBpsList] = useState([]);
+  const [bpsList, setBpsList] = useState([{key: "id12312312", value: [1, 0, 1, 0, 1, 0, 0, 1]}]);
   const [bps, setBps] = useState([false, false, false, false, false, false, false, false]);
   const [user, setUser] = useState(null);
   const [playStatus, setPlayStatus] = useState(true);
   const [interval, setInter] = useState(null);
+  const [dataToSave, setDataToSave] = useState(null);
+  const [indicator, setIndicator] = useState(1);
 
   const getBpsList = async () => {
     try {
+      let bpsFromDb = [];
       // get list axios
-      setBpsList([]);
+      const storageKeys = await bridge.send("VKWebAppStorageGetKeys", {"count": 1000, "offset": 0});
+      const storageValues = await bridge.send("VKWebAppStorageGet", {"keys": storageKeys.keys});
+      let keysToRender = storageValues.keys.map(item => {
+        return {key: item.key, value: item.value}
+      });
+
+      setBpsList([...bpsList, ...keysToRender]);
+      console.log('BPSawdawdaw', bpsList);
     } catch (error) {
       console.log('QrCount ERROR', error.message);
     }
@@ -37,7 +59,7 @@ const BitLight = props => {
       setInter(null);
     }
 
-      console.log('playStatus awdawdaw', playStatus);
+    console.log('playStatus awdawdaw', playStatus);
     bridge.send('VKWebAppFlashGetInfo')
         .then(flashData => console.log(flashData))
         .catch(error => console.log(error));
@@ -66,6 +88,22 @@ const BitLight = props => {
     console.log(bits);
   };
 
+  useEffect(() => {
+    setIndicator(user && "id" + user.id + Date.now());
+  }, [dataToSave]);
+
+  const saveBpsHandler = () => {
+    const data = {
+      "key": indicator,
+      "value": JSON.stringify(bps),
+    };
+    setDataToSave(data);
+    console.log('save', data);
+    bridge.send("VKWebAppStorageSet", data)
+        .then(saved => console.log('SAVED', saved))
+        .catch(error => console.log('SAVED ERROR', error))
+  };
+
   return (
       <Panel id={props.id}>
         <PanelHeader
@@ -80,7 +118,7 @@ const BitLight = props => {
           </Div>
         </Group>
         <Group>
-          <Header mode="secondary" indicator={user && "id" + user.id + Date.now()}>Создать свой BPS</Header>
+          <Header mode="secondary" indicator={indicator}>Создать свой BPS</Header>
           <Div style={{display: 'flex', justifyContent: "center", height: '55px'}}>
             <IconButton style={{margin: '-4px'}} data-id={0} onClick={(e) => bitHandler(e)}>
               {!bps[0] ? <Icon16Add/> : <Icon16Cancel/>}1s</IconButton>
@@ -101,7 +139,7 @@ const BitLight = props => {
           </Div>
           <Div style={{display: 'flex'}}>
             <Button
-                before={!playStatus ? <Icon28Play/> : <Icon24Stop/>}
+                before={playStatus ? <Icon28Play/> : <Icon24Stop/>}
                 mode="outline"
                 size="l"
                 onClick={() => {
@@ -113,11 +151,23 @@ const BitLight = props => {
             >{!playStatus ? 'Остановить' : 'Воспроизвести'}</Button>
           </Div>
           <Div style={{display: 'flex'}}>
-            <Button before={<Icon28VinylOutline/>} size="l" stretched style={{marginRight: 0}}>Создать</Button>
+            <Button
+                before={<Icon28VinylOutline/>}
+                size="l"
+                onClick={() => saveBpsHandler()}
+                stretched style={{marginRight: 0}
+            }>Создать</Button>
           </Div>
         </Group>
         <Group>
           <Header mode="secondary" indicator={bpsList ? bpsList.length : 0}>Список сторонних BPS</Header>
+          <List>
+            {bpsList && bpsList.map((item, index) => {
+                  return <Cell key={index} onClick={() => setBps(item.value)} before={<Icon16Add/>}>{item.key}</Cell>
+                }
+            )
+            }
+          </List>
         </Group>
       </Panel>
   );
